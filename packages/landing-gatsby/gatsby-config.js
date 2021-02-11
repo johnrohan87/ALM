86cdf99618f960510b00ebe6163deafcdaa54fa4
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2021-02-10 22:38:00 -0500

    setting up google analytics

--- packages/landing-gatsby/gatsby-config.js
+++ packages/landing-gatsby/gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "GA_TRACKING_ID", // Google Analytics / GA
+          process.env.GATSBY_GA_TRACKING_ID, // Google Analytics / GA
           "AW-CONVERSION_ID", // Google Ads / Adwords / AW
           "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
         ],
