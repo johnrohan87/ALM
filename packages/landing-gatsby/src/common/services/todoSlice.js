2fefed7ed6ae08d3423ce65bcc766a7b76f33642
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2024-02-08 09:30:58 -0500

    added todoSlice

---
+++ packages/landing-gatsby/src/common/services/todoSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchTodos } from '../contexts/AxiosContext';
+
+const initialState = {
+  todos: [],
+  loading: false,
+  error: null,
+};
+
+const todoSlice = createSlice({
+  name: 'todos',
+  initialState,
+  reducers: {
+    // Add reducers for other actions if needed
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchTodos.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchTodos.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.todos = action.payload;
+      })
+      .addCase(fetchTodos.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default todoSlice.reducer;
