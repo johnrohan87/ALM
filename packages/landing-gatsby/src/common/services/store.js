b2cf5748c8cf59b98c761e9b0aef3bb198253ca0
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2024-02-08 09:58:16 -0500

    added slice

--- packages/landing-gatsby/src/common/services/store.js
+++ packages/landing-gatsby/src/common/services/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
+import todoReducer from './todoSlice';
 
 export default configureStore({
   reducer: {
     user: userReducer,
+    todos: todoReducer,
   },
 });
