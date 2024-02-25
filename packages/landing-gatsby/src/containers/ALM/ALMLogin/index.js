c14e976cb29ec4663f76af9d3c9e40dc853a8283
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2024-02-25 14:32:11 -0500

    added imports needed in login

--- packages/landing-gatsby/src/containers/ALM/ALMLogin/index.js
+++ packages/landing-gatsby/src/containers/ALM/ALMLogin/index.js
@@ -1,31 +1,28 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { navigate } from 'gatsby';
-import {
-  isTokenFresh,
-  refreshToken,
-} from '../../../common/contexts/AxiosContext';
-import { getUser, isLoggedIn } from '../../../common/services/auth';
-import { Provider, useSelector, useDispatch } from 'react-redux';
+import { Provider } from 'react-redux';
 import {
   fetchLoginData,
   fetchUserData,
   set_userinfo,
 } from '../../../common/services/userSlice';
 import store from '../../../common/services/store';
-//import thunk from 'redux-thunk';
+import {
+  isTokenFresh,
+  refreshToken,
+} from '../../../common/contexts/AxiosContext';
 
 const ALMLogin = ({ state }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const dispatch = useDispatch();
-  //const [loading, setLoading] = useState(user.loading ? user.loading : false);
-  //const [loggedIn, setLoggedIn] = useState(user.logged_in ? user.logged_in : false);
-  //const user = useSelector(state => state.user);
+  const isLoggedIn = useSelector((state) => state.user.logged_in);
 
-  const FetchToken = async ({ email, password }) => {
+  const fetchToken = async ({ email, password }) => {
     await dispatch(fetchLoginData({ email, password }));
-    if (isLoggedIn()) {
+    if (isLoggedIn) {
       let isFresh = false;
       try {
         isFresh = await isTokenFresh();
@@ -48,77 +45,74 @@ const ALMLogin = ({ state }) => {
   };
 
   const handleClick = (email, password) => {
-    FetchToken({ email, password });
+    fetchToken({ email, password });
   };
 
   return (
-    <>
-      <Provider store={store}>
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
-              <div className="card border-0 shadow rounded-3 my-5">
-                <div className="card-body p-4 p-sm-5">
-                  <h5 className="card-title text-center mb-5 fw-light fs-5">
-                    Sign In
-                  </h5>
-                  <p>{store.logged_in}</p>
-                  <div>
-                    <div className="form-floating mb-3">
-                      <input
-                        type="email"
-                        className="form-control"
-                        id="floatingInput"
-                        placeholder="name@example.com"
-                        onChange={(e) => {
-                          setEmail(e.target.value);
-                        }}
-                      />
-                      <label for="floatingInput">Email address</label>
-                    </div>
-                    <div className="form-floating mb-3">
-                      <input
-                        type="password"
-                        className="form-control"
-                        id="floatingPassword"
-                        placeholder="Password"
-                        onChange={(e) => {
-                          setPassword(e.target.value);
-                        }}
-                      />
-                      <label for="floatingPassword">Password</label>
-                    </div>
+    <Provider store={store}>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+            <div className="card border-0 shadow rounded-3 my-5">
+              <div className="card-body p-4 p-sm-5">
+                <h5 className="card-title text-center mb-5 fw-light fs-5">
+                  Sign In
+                </h5>
+                <div>
+                  <div className="form-floating mb-3">
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="floatingInput"
+                      placeholder="name@example.com"
+                      onChange={(e) => {
+                        setEmail(e.target.value);
+                      }}
+                    />
+                    <label htmlFor="floatingInput">Email address</label>
+                  </div>
+                  <div className="form-floating mb-3">
+                    <input
+                      type="password"
+                      className="form-control"
+                      id="floatingPassword"
+                      placeholder="Password"
+                      onChange={(e) => {
+                        setPassword(e.target.value);
+                      }}
+                    />
+                    <label htmlFor="floatingPassword">Password</label>
+                  </div>
 
-                    <div className="form-check mb-3">
-                      <input
-                        className="form-check-input"
-                        type="checkbox"
-                        value=""
-                        id="rememberPasswordCheck"
-                      />
-                      <label
-                        className="form-check-label"
-                        for="rememberPasswordCheck"
-                      >
-                        Remember password
-                      </label>
-                    </div>
-                    <div className="d-grid">
-                      <button
-                        className="btn btn-primary btn-login text-uppercase fw-bold"
-                        onClick={() => handleClick(email, password)}
-                      >
-                        Sign in
-                      </button>
-                    </div>
+                  <div className="form-check mb-3">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      value=""
+                      id="rememberPasswordCheck"
+                    />
+                    <label
+                      className="form-check-label"
+                      htmlFor="rememberPasswordCheck"
+                    >
+                      Remember password
+                    </label>
+                  </div>
+                  <div className="d-grid">
+                    <button
+                      className="btn btn-primary btn-login text-uppercase fw-bold"
+                      onClick={() => handleClick(email, password)}
+                    >
+                      Sign in
+                    </button>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </Provider>
-    </>
+      </div>
+    </Provider>
   );
 };
 
